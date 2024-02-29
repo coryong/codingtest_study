69abe9169a19e2b5e2d50314ccbb566734ea809e
kihokwon <54932415+kihokwon@users.noreply.github.com>
2024-02-29 21:57:17 +0900

    Update [Baekjoon-11047]동전0.py
    completed

--- Baekjoon/Kwon/[Baekjoon-11047]동전0.py
+++ Baekjoon/Kwon/[Baekjoon-11047]동전0.py
@@ -1,1 +1,15 @@
-Test
+import sys
+N, K = map(int, sys.stdin.readline().split())
+coins = [int(sys.stdin.readline().rstrip()) for i in range(N)]
+
+#
+cnt = 0
+for coin in coins[::-1]:
+    if K // coin > 0:
+        cnt += K // coin
+        K %= coin
+    if K <= 0:
+        break
+
+print(cnt)
+
