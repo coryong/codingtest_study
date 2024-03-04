36b7a4addeea06702c5ffb6993586b59c7b5d5b3
parkinha <qaqrs99@gmail.com>
2024-03-04 15:45:30 +0900

    [Test-0304]박인하

---
+++ 코딩테스트/Park/[3.4(월)]/구명보트.py
@@ -0,0 +1,11 @@
+def solution(people, limit):
+    people.sort()
+    answer = 0
+    i=0
+    j=len(people)-1
+    while i<=j:
+        if people[i]+people[j]<=limit:
+            i=i+1
+        j-=1
+        answer+=1
+    return answer
