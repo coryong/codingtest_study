0bc87113b9072ec79992b59d1562449f8760a915
LEE SNAG-WOO <113656612+strokeincoding@users.noreply.github.com>
2024-03-04 21:09:25 +0900

    [Beakjoon-2839]설탕배달

---
+++ Baekjoon/Lee/[Beakjoon-2839]설탕배달.py
@@ -0,0 +1,17 @@
+n=int(input())
+cnt=0
+while n>0:
+    if n % 5 ==0:
+        n -=5
+        cnt += 1
+    elif n% 3==0:
+        n -=3
+        cnt += 1
+    else:
+        n -= 5
+        cnt +=1
+        
+if n==0:
+    print(cnt)
+else:
+    print(-1)  
