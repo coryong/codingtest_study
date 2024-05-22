9829713f2db45885c28b3a4a519f81ef33a98ce5
kihokwon <54932415+kihokwon@users.noreply.github.com>
2024-05-22 16:18:37 +0900

    Update Q3_문자열 뒤집기.py

--- Book/Kwon/greedy/Q3_문자열 뒤집기.py
+++ Book/Kwon/greedy/Q3_문자열 뒤집기.py
@@ -1,25 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-# 1과 0중에 뭐가 많은지 체크하고
-# 연속적으로 뒤집는 수를 센다
 def solution(s):
-    cnt = 0
-    result = 0
-    for i in s:
-        cnt = cnt + 1 if cnt == '0' else cnt - 1
-    # 0이 많은 경우
-    if cnt >= 0:
-        for i in range(len(s)-1):
-            if s[i] == '1' and s[i+1] == '0':
-                result += 1
-    # 1이 많은 경우
+    cnt0, cnt1 = 0, 0
+    if s[0] == '1':
+        cnt0 += 1
     else:
-        for i in range(len(s)-1):
-            if s[i] == '0' and s[i+1] == '1':
-                result += 1
-    
-    return result
+        cnt1 += 1
+    # 처음에 다르게 계산했다가 바로 오답
+    for i in range(len(s)-1):
+        if s[i] != s[i+1]:
+            if s[i+1] == '0':
+                cnt1 += 1
+            else:
+                cnt0 += 1
+
+    return min(cnt0, cnt1)
 
 s = list(input().rstrip())
 print(solution(s))
