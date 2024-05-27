4017dfddaa84eef11e68f23d320954b8d3f18f0e
BOO-WONUK <wkb0307@naver.com>
2024-05-27 11:10:51 +0900

    [Test-0527] 부원국

--- 코딩테스트/Boo/[5.27(월)]/크리스마스 선물.py
+++ 코딩테스트/Boo/[5.27(월)]/크리스마스 선물.py
@@ -16,16 +16,3 @@ for _ in range(n):
         for i in k:
             heapq.heappush(lst,-i)
     
-# for _ in range(n):
-#     a = input()
-#     if a == '0':
-#         if len(lst) == 0:
-#             print(-1)
-#         else:
-#             print(lst.pop(0))
-#         continue
-#     else:
-#         k = list(map(int,a.split()))[1:]
-#         for i in k:
-#             lst.append(i)
-    
