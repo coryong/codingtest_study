d93974a183349b7103b89590e0815528aeeb427e
kihokwon <54932415+kihokwon@users.noreply.github.com>
2024-05-31 15:36:15 +0900

    Create 2696_중앙값구하기.py

---
+++ 추가문제/Kwon/Baekjoon/PriorityQueue/2696_중앙값구하기.py
@@ -0,0 +1,45 @@
+import sys
+import heapq as hq
+input = sys.stdin.readline
+# RuntimeError 한번 발생함. -> temp로 리스트와 힙을 계속 만들면 시간복잡도에서 걸림
+# 매번 전체 리스트를 슬라이싱하여 새로운 힙을 생성하는 것은 비효율적이며, 특히 리스트가 클 경우 메모리 초과가 발생
+
+import sys
+import heapq as hq
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    m = int(input())
+    nums = []
+    while len(nums) < m:
+        nums.extend(map(int, input().split()))
+    
+    max_heap = []
+    min_heap = []  
+    result = []
+    
+    for i in range(m):
+        num = nums[i]
+        
+        # 최대 힙에 먼저 추가
+        if len(max_heap) == 0 or num <= -max_heap[0]:
+            hq.heappush(max_heap, -num)
+        else:
+            hq.heappush(min_heap, num)
+        
+        # 힙의 크기 조정
+        if len(max_heap) > len(min_heap) + 1:
+            hq.heappush(min_heap, -hq.heappop(max_heap))
+        elif len(min_heap) > len(max_heap):
+            hq.heappush(max_heap, -hq.heappop(min_heap))
+        
+        # i가 홀수일 때마다 중간값을 추가
+        if i % 2 == 0:
+            result.append(-max_heap[0])
+    
+    print(len(result))
+    print(*result)
+
+
+
